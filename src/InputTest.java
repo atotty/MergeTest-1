38a9fa76034d0c5a40aa896418a952b75a4c2e29
Austin <awtotty@gmail.com>
2018-09-10 12:28:55 -0700

    Initial

---
+++ src/InputTest.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class InputTest {
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+
+        kb.nextInt();
+        kb.close();
+    }
+}
